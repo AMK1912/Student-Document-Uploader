93cf6090906e867f6b30aafb70edda62438e380e
Amir Mustafa Khan <91549914+AMK1912@users.noreply.github.com>
2022-02-09 15:31:09 +0530

    Update __init__.py

--- website/__init__.py
+++ website/__init__.py
@@ -27,37 +27,37 @@ def create_app():
     #driver= 'ODBC Driver 17 for SQL Server'
     
     # Configure Database URI: 
-        params = urllib.parse.quote_plus(
-            'Driver=%s;' % 'ODBC Driver 17 for SQL Server' +
-            'Server=tcp:%s,1433;' % 'updoc.database.windows.net' +
-            'Database=%s;' % 'UpDocSQL' +
-            'Uid=%s;' % 'amir' +
-            'Pwd={%s};' % 'amk@1912' +
-            'Encrypt=yes;' +
-            'TrustServerCertificate=no;' +
-            'Connection Timeout=30;')
+    params = urllib.parse.quote_plus(
+        'Driver=%s;' % 'ODBC Driver 17 for SQL Server' +
+        'Server=tcp:%s,1433;' % 'updoc.database.windows.net' +
+        'Database=%s;' % 'UpDocSQL' +
+        'Uid=%s;' % 'amir' +
+        'Pwd={%s};' % 'amk@1912' +
+        'Encrypt=yes;' +
+        'TrustServerCertificate=no;' +
+        'Connection Timeout=30;')
 
-        # Initialize application with URI for Azure DB
-        app = Flask(__name__)
-        app.config['SECRET_KEY'] = 'dfvovniv bfuo3bfui3fih'
-        app.config['SQLALCHEMY_DATABASE_URI'] = "mssql+pyodbc:///?odbc_connect=%s" % params
-        app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
-        app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    # Initialize application with URI for Azure DB
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'dfvovniv bfuo3bfui3fih'
+    app.config['SQLALCHEMY_DATABASE_URI'] = "mssql+pyodbc:///?odbc_connect=%s" % params
+    app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-        db.init_app(app)
+    db.init_app(app)
 
-        from .views import views
-        from .auth import auth
+    from .views import views
+    from .auth import auth
 
-        app.register_blueprint(views, url_prefix='/')
-        app.register_blueprint(auth, url_prefix='/')
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
 
-        from .models import Student, Documents
+    from .models import Student, Documents
 
-        login_manager=LoginManager()
-        login_manager.login_view = 'auth.login'
-        login_manager.init_app(app)
-   
+    login_manager=LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
 
     @login_manager.user_loader
     def load_user(Id):
