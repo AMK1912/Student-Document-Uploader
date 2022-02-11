e2e4c5fe30b3c297273adfac9173b4e563d96bb9
Amir Mustafa Khan <91549914+AMK1912@users.noreply.github.com>
2022-02-11 22:03:22 +0530

    Update views.py

--- website/views.py
+++ website/views.py
@@ -6,4 +6,4 @@ views = Blueprint('views',__name__)
 @views.route('/', methods=['GET', 'POST'])
 @login_required
 def home():
-    return render_template("home.html", user=current_user)
+    return render_template("Home.html", user=current_user)
